5c5164a6837b7a316d7de8a3018f15a029175c85
Jackson Visser <jvisser@pugetsound.edu>
2021-11-14 14:02:36 -0800

    solution generates offline

--- app.js
+++ app.js
@@ -4,6 +4,14 @@
 
 const START_LEGNTH = 12;
 
+//grab nodes up top
+const red = document.getElementById("redSq");
+const blue = document.getElementById("blueSq");
+const yellow = document.getElementById("yellowSq");
+const green = document.getElementById("greenSq");
+const play = document.getElementById("play");
+const roundsText = document.getElementById("rounds");
+
 //generate start sequence offline for now
 function getStartSequence() {
   let startObj = { type: "start", sequence: [] };
@@ -25,4 +33,27 @@ function getStartSequence() {
     }
   }
   return startObj;
+}
+
+//also generate solution offline for now
+function getSolution(rounds) {
+  let solutionObj = { type: "solution", key: [] };
+  for (let i = 0; i < rounds; i++) {
+    let num = Math.floor(Math.random() * 4 + 1);
+    switch (num) {
+      case 1:
+        solutionObj.key.push("R");
+        break;
+      case 2:
+        solutionObj.key.push("B");
+        break;
+      case 3:
+        solutionObj.key.push("Y");
+        break;
+      case 4:
+        solutionObj.key.push("G");
+        break;
+    }
+  }
+  return solutionObj;
 }
