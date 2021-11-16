cc250ca43e0efb2af24e974e5e7a7c8febe73cb0
Jackson Visser <jvisser@pugetsound.edu>
2021-11-15 19:44:22 -0800

    cleaned up code and added commments

--- bundle.js
+++ bundle.js
@@ -17,21 +17,25 @@ const status = document.getElementById("status");
 //field to represent Simone game
 let game;
 
-//class to represent a game of Simone (might try a diff approach later)
+//class to represent a game of Simone
 class Simone {
-  won = false;
-  lost = false;
   currRound = 1;
   roundsTotal;
   solution;
-  inputs = [];
+  inputs = []; //an array to keep track of user inputs
   constructor(rounds = 10, solution) {
     this.roundsTotal = rounds;
     this.solution = solution;
   }
+  /* 
+  displays the opening sequence
+  @param seq A vaild sequence array to display
+  */
   async displayStart(seq) {
     for (let i = 0; i < seq.length; i++) {
-      switch (seq[i]) {
+      switch (
+        seq[i] //find out which button to light up by adding, then removing light color class
+      ) {
         case "R":
           red.classList.add("lightred");
           new Audio("sounds/red.wav").play();
@@ -73,6 +77,7 @@ class Simone {
           green.classList.remove("lightgreen");
           break;
       }
+      //delay between each button lighting up
       await new Promise((resolve) =>
         setTimeout(() => {
           resolve();
@@ -80,10 +85,15 @@ class Simone {
       );
     }
   }
-
+  /*
+  displays a portion of the solution to the user. the portion displayed increases as the game goes on
+  @param seq A vaild sequence array to display
+  */
   async displaySoulution(seq) {
     for (let i = 0; i < this.currRound; i++) {
-      switch (seq[i]) {
+      switch (
+        seq[i] //find out which button to light up by adding, then removing light color class
+      ) {
         case "R":
           red.classList.add("lightred");
           new Audio("sounds/red.wav").play();
@@ -125,6 +135,7 @@ class Simone {
           green.classList.remove("lightgreen");
           break;
       }
+      //delay between displaying each button
       await new Promise((resolve) =>
         setTimeout(() => {
           resolve();
@@ -132,13 +143,17 @@ class Simone {
       );
     }
   }
-
+  /*
+  displays a status message to the user
+  @param msg the message to display
+  */
   async displayStatus(msg) {
     status.innerHTML = msg;
   }
-
+  /*
+  main method responsible for a turn of gameplay. called whenever a button is pressed
+  */
   async updateGame() {
-    //should be called everytime a button is clicked. buttons add inputs
     //first, find out if user input is right or wrong
     let correct = true;
     for (let i = 0; i < this.inputs.length; i++) {
@@ -156,7 +171,7 @@ class Simone {
       //display "you lose" status
       this.displayStatus("Incorrect! You lose!");
     } else {
-      //they are correct. figure out the scenario
+      //they are correct. figure out what scenario it is
       //1. game ends if last button in last round
       if (
         this.currRound == this.roundsTotal &&
@@ -174,9 +189,9 @@ class Simone {
         //update status
         this.displayStatus("Good job! Prepare for next round.");
         new Audio("sounds/nextRound.wav").play();
-        //update round
+        //increment round
         this.currRound++;
-        //clear inputs
+        //clear inputs and delay between rounds
         this.inputs.splice(0, this.inputs.length);
         await new Promise((resolve) =>
           setTimeout(() => {
@@ -218,20 +233,28 @@ class Simone {
   }
 
   //setters
+  /*
+  adds a user input to the end of the input array
+  @param input the input to add
+  */
   addInput(input) {
     this.inputs.push(input);
   }
 }
 
-//clicking on the "Play Simone" button should instantiate a new Simone game with the correct number of rounds
+//clicking on the "Play Simone" button starts the game
 play.addEventListener("click", async function () {
   if (roundsText.value == "") {
+    //if no value, use default (10)
     let sol = await getSolutionAPI(10);
     game = new Simone(10, sol);
   } else {
+    //otherwise, use the value from user
     let sol = await getSolutionAPI(roundsText.value);
     game = new Simone(roundsText.value, sol);
   }
+
+  //get start sequence  and display
   let start = await getStartAPI();
   game.displayStart(start);
   await new Promise((resolve) =>
@@ -239,6 +262,8 @@ play.addEventListener("click", async function () {
       resolve();
     }, 4000)
   );
+
+  //display intial status and solution to the user
   game.displayStatus(`Round 1 of ${game.getTotalRounds()}`);
   game.displaySoulution(game.getSolution());
 });
@@ -269,7 +294,7 @@ green.addEventListener("click", function () {
   game.updateGame();
 });
 
-//buttons light up on click down
+//these listeners make buttons light up on click down
 red.addEventListener("mousedown", function () {
   red.classList.toggle("lightred");
 });
@@ -286,7 +311,7 @@ green.addEventListener("mousedown", function () {
   green.classList.toggle("lightgreen");
 });
 
-//buttons are no longer lit when mouse moved away
+//thse listeners make buttons no longer lit when mouse moved away
 red.addEventListener("mouseleave", function () {
   red.classList.remove("lightred");
 });
@@ -303,7 +328,7 @@ green.addEventListener("mouseleave", function () {
   green.classList.remove("lightgreen");
 });
 
-//play sound when mouse is lifted and remove light color
+//thse listeners play appropriate sound when mouse is lifted and remove light color
 red.addEventListener("mouseup", function () {
   red.classList.remove("lightred");
   new Audio("sounds/red.wav").play();
@@ -324,7 +349,7 @@ green.addEventListener("mouseup", function () {
   new Audio("sounds/green.wav").play();
 });
 
-//add event listeners needed for all buttons
+//these two listeners get added to all buttons so they get white borders when hovered over
 for (let b of btns) {
   //light up on hover
   b.addEventListener("mouseenter", function () {
@@ -335,69 +360,28 @@ for (let b of btns) {
   });
 }
 
-//generate start sequence offline for now
-function getStart() {
-  let startObj = { type: "start", sequence: [] };
-  for (let i = 0; i < START_LEGNTH; i++) {
-    let num = Math.floor(Math.random() * 4 + 1);
-    switch (num) {
-      case 1:
-        startObj.sequence.push("R");
-        break;
-      case 2:
-        startObj.sequence.push("B");
-        break;
-      case 3:
-        startObj.sequence.push("Y");
-        break;
-      case 4:
-        startObj.sequence.push("G");
-        break;
-    }
-  }
-  return startObj;
-}
-
-//also generate solution offline for now
-function getSolution(rounds) {
-  let solutionObj = { type: "solution", key: [] };
-  for (let i = 0; i < rounds; i++) {
-    let num = Math.floor(Math.random() * 4 + 1);
-    switch (num) {
-      case 1:
-        solutionObj.key.push("R");
-        break;
-      case 2:
-        solutionObj.key.push("B");
-        break;
-      case 3:
-        solutionObj.key.push("Y");
-        break;
-      case 4:
-        solutionObj.key.push("G");
-        break;
-    }
-  }
-  return solutionObj.key;
-}
-
+//grabs the start sequence from the api
 async function getStartAPI() {
   try {
     let response = await axios.get(
       "http://cs.pugetsound.edu/~dchiu/cs240/api/simone/?cmd=start"
     );
-    return response.data.sequence;
+    return response.data.sequence; //extract the array
   } catch (error) {
     console.error(error);
   }
 }
 
+/*
+grabs the solution sequence from the api
+@param rounds The number of rounds to play / length of solution sequence
+*/
 async function getSolutionAPI(rounds) {
   try {
     let response = await axios.get(
       `http://cs.pugetsound.edu/~dchiu/cs240/api/simone/?cmd=getSolution&rounds=${rounds}`
     );
-    return response.data.key;
+    return response.data.key; //extract the array
   } catch (error) {
     console.error(error);
   }
