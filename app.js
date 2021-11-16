7cd3679791574d0650ee5fba9ab3a949ab89b26a
Jackson Visser <jvisser@pugetsound.edu>
2021-11-15 16:37:25 -0800

    nvm, i lost my work. redoing

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 @author Jackson Visser
  */
 
-const START_LEGNTH = 12;
-
 //grab nodes up top
 const red = document.getElementById("redSq");
 const blue = document.getElementById("blueSq");
@@ -223,52 +221,6 @@ class Simone {
   }
 }
 
-//generate start sequence offline for now
-function getStart() {
-  let startObj = { type: "start", sequence: [] };
-  for (let i = 0; i < START_LEGNTH; i++) {
-    let num = Math.floor(Math.random() * 4 + 1);
-    switch (num) {
-      case 1:
-        startObj.sequence.push("R");
-        break;
-      case 2:
-        startObj.sequence.push("B");
-        break;
-      case 3:
-        startObj.sequence.push("Y");
-        break;
-      case 4:
-        startObj.sequence.push("G");
-        break;
-    }
-  }
-  return startObj;
-}
-
-//also generate solution offline for now
-function getSolution(rounds) {
-  let solutionObj = { type: "solution", key: [] };
-  for (let i = 0; i < rounds; i++) {
-    let num = Math.floor(Math.random() * 4 + 1);
-    switch (num) {
-      case 1:
-        solutionObj.key.push("R");
-        break;
-      case 2:
-        solutionObj.key.push("B");
-        break;
-      case 3:
-        solutionObj.key.push("Y");
-        break;
-      case 4:
-        solutionObj.key.push("G");
-        break;
-    }
-  }
-  return solutionObj.key;
-}
-
 //clicking on the "Play Simone" button should instantiate a new Simone game with the correct number of rounds
 play.addEventListener("click", async function () {
   if (roundsText.value == "") {
@@ -376,4 +328,50 @@ for (let b of btns) {
   b.addEventListener("mouseleave", function () {
     b.classList.toggle("hover");
   });
+}
+
+//generate start sequence offline for now
+function getStart() {
+  let startObj = { type: "start", sequence: [] };
+  for (let i = 0; i < START_LEGNTH; i++) {
+    let num = Math.floor(Math.random() * 4 + 1);
+    switch (num) {
+      case 1:
+        startObj.sequence.push("R");
+        break;
+      case 2:
+        startObj.sequence.push("B");
+        break;
+      case 3:
+        startObj.sequence.push("Y");
+        break;
+      case 4:
+        startObj.sequence.push("G");
+        break;
+    }
+  }
+  return startObj;
+}
+
+//also generate solution offline for now
+function getSolution(rounds) {
+  let solutionObj = { type: "solution", key: [] };
+  for (let i = 0; i < rounds; i++) {
+    let num = Math.floor(Math.random() * 4 + 1);
+    switch (num) {
+      case 1:
+        solutionObj.key.push("R");
+        break;
+      case 2:
+        solutionObj.key.push("B");
+        break;
+      case 3:
+        solutionObj.key.push("Y");
+        break;
+      case 4:
+        solutionObj.key.push("G");
+        break;
+    }
+  }
+  return solutionObj.key;
 }
